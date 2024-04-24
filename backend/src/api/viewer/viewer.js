d1947387c230632a72a76a3ea5b10f4472cdf583
Cédric Champeix <cedric.rchampeix@gmail.com>
2024-04-24 21:22:19 +0100

    Changed endpoint to get a users resume from the viewer

--- backend/src/api/viewer/viewer.js
+++ backend/src/api/viewer/viewer.js
@@ -1,9 +1,9 @@
 const app = require("express")
-const {getInformation, getResume, getPortfolio, getProject} = require("../../service/viewer")
+const {getResume, getPortfolio, getProject} = require("../../service/viewer")
 
 let router = new app.Router()
 
-router.route("/resume/:username").get(getResume)
+router.route("/:username/resume").get(getResume)
 
 router.route("/:username/projects").get(getPortfolio)
 router.route("/:username/projects/:projectId").get(getProject)
